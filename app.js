33f5a921caaa2eeb9651041d1ab0b4d86ded0a14
Mazen Sadek <mazend2242@gmail.com>
2024-12-18 17:41:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+let title = document.getElementById("title");
+let description = document.getElementById("description");
+let plan = document.getElementById("plan");
+let proccedbtn = document.getElementById("procced");
+let cancellbtn = document.getElementById("cancel");
+let goHome = document.getElementById("goHome");
+let changebtn = document.getElementById("change");
+let changeTitle = document.getElementById("changeTitle");
+let changePrice = document.getElementById("changePrice");
+let confirming = document.getElementById("loading");
+
+function procced() {
+  setTimeout(() => {
+    confirming.style.display = "none";
+  }, 5000);
+
+  confirming.style.display = "flex";
+  title.innerText = "Succesfully Subscribed";
+  description.style.display = "none";
+  plan.style.display = "none";
+  proccedbtn.style.display = "none";
+  cancellbtn.style.display = "none";
+  goHome.style.display = "block";
+  goHome.ariaHidden = false;
+}
+
+function cancel() {
+  title.innerText = "Subscription Cancelled";
+  description.style.display = "none";
+  plan.style.display = "none";
+  proccedbtn.style.display = "none";
+  cancellbtn.style.display = "none";
+  goHome.style.display = "block";
+}
+
+const plans = [
+  { name: "Annual Plan", price: "$59.99/year" },
+  { name: "Daily Plan", price: "$5.99/day" },
+  { name: "Monthly Plan", price: "$29.99/month" },
+];
+
+let currentIndex = 0;
+
+function change() {
+  currentIndex = (currentIndex + 1) % plans.length;
+  changeTitle.innerText = plans[currentIndex].name;
+  changePrice.innerText = plans[currentIndex].price;
+}
